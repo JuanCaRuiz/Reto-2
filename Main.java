816584abdd41bc0ebd4f89957f12d7ddf9990a79
JuanCRuiz <60896331+JuanCaRuiz@users.noreply.github.com>
2021-07-26 19:05:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    /*
+    La Universidad El Bosque en su papel de acompañamiento a instituciones que requieran su colaboración ha decidido apoyar en el proceso de actualización de datos al Instituto La Floresta. En este proceso se ha identificado la necesidad de tener un directorio único para el manejo de los datos de contacto de los estudiantes. 
+    Para este fin, se ha consolidado que los datos de los estudiantes que se consideran importantes a trabajar son:
+      • Nombres
+      • Apellidos
+      • Fecha de nacimiento
+      • Correo institucional
+      • Correo personal
+      • Número telefónico celular
+      • Número telefónico fijo
+      • Programa académico al cual pertenece
+    La aplicación debe permitir el CRUD completo (Crear, Leer, Actualizar, Eliminar) para los estudiantes, debe manejar DTO, debe incorporarse el manejo de excepciones y la documentación de código necesaria, es importante utilizar el MVC visto en clases anteriores.
+    */
+
+		Vista.menu();
+  }
+}
---
+++ Vista.java
@@ -0,0 +1,13 @@
+public class Vista {
+
+    public static void menu(){
+        System.out.println("INSTITUTO LA FLORESTA");
+        System.out.println("Seleccione tarea a realizar:");
+        System.out.println("1. Ingresar estudiante");
+        System.out.println("2. Buscar estudiante");
+        System.out.println("3. Modificar estudiante");
+        System.out.println("4. Eliminar Estudiante");
+        System.out.println("5. Ver directorio de estudiantes");
+        System.out.println("6. Salir");
+    }
+}
